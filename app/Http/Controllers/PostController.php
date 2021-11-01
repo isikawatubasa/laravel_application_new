d179d9ceed36c9b970068a54bf115a7bebe3d5c2
isikawatubasa <92654985+isikawatubasa@users.noreply.github.com>
2021-11-01 15:36:27 +0900

    add Laravel_Q7

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;//Postモデルを使う
+use Illuminate\Support\Facades\Auto;//ログイン機能を使う
+class PostController extends Controller
+{
+    public function index()//投稿一覧画面表示
+    {
+        $posts = Post::all;//モデルから投稿を全件取得として表示する
+        return view('posts.index', ['posts' => $posts]);//取得したデータをビューに変数として渡す
+    }
+
+    public function create()//登録画面表示（投稿）
+    {
+        return view('posts.create');//create.blade.phpを表示する（これを作成）
+    }
+
+    public function store(PostRequest $request)//登録処理（投稿）
+    {
+        $post = new post;//postのインスタンスを生成//ユーザーが入力したリクエストの情報を格納していく
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->user_id = Auth::id();//Auth::id()でログインユーザーのIDが取得できる
+        $post->save();//インスタンスをDBのレコードとして保存
+        return redirect()->route('post.index');//投稿一覧画面にれダイレクトさせる
+    }
+}
