e784971ac24c76361074bd54d0424b6802532883
isikawatubasa <92654985+isikawatubasa@users.noreply.github.com>
2021-11-03 13:21:15 +0900

    add laravelQ9_2 show

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,7 +19,7 @@
                             {{ $post->body }}
                         </p>
                         <div class="text-right">
-                            <a class="btn btn-primary btn-sm" href="{{ route('post.shoe', ['id' => $post->id]) }}">
+                            <a class="btn btn-primary btn-sm" href="{{ route('post.show', ['id' => $post->id]) }}">
                                 <i class="far fa-show"></i>詳細
                             </a>
                         </div>
